1202389110900c7611b50efd36e8295bb2220e7e
Chan Ci En <chance3009@outlook.com>
2024-12-06 00:25:44 +0800

    Update build.sh - 5

--- build.sh
+++ build.sh
@@ -2,40 +2,34 @@
 # exit on error
 set -o errexit
 
-# Define storage directory for Render environment
-# Install Chrome (as before)
 STORAGE_DIR=/opt/render/project/.render
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
-    echo "...Downloading Chrome"
-    mkdir -p $STORAGE_DIR/chrome
-    cd $STORAGE_DIR/chrome
-    wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-    rm ./google-chrome-stable_current_amd64.deb
-    cd $HOME/project/src
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
 else
-    echo "...Using Chrome from cache"
+  echo "...Using Chrome from cache"
 fi
 
-# Get the Chrome version
+# Fetch Chrome version and use it to get the corresponding ChromeDriver
 CHROME_VERSION=$($STORAGE_DIR/chrome/opt/google/chrome/chrome --version | awk '{print $3}')
-echo "Chrome Version: $CHROME_VERSION"  # Log the version to check if it's correct
+echo "Chrome Version: $CHROME_VERSION"
 
-# Now fetch the correct ChromeDriver version for this Chrome version
+# Fetch the corresponding ChromeDriver version
 DRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-echo "ChromeDriver Version: $DRIVER_VERSION"  # Log the driver version to check if it's correct
+echo "ChromeDriver Version: $DRIVER_VERSION"
 
-# Download ChromeDriver
-wget -P $STORAGE_DIR/chrome/ https://chromedriver.storage.googleapis.com/$DRIVER_VERSION/chromedriver_linux64.zip
-unzip $STORAGE_DIR/chrome/chromedriver_linux64.zip -d $STORAGE_DIR/chrome/
+# Download the corresponding ChromeDriver
+wget "https://chromedriver.storage.googleapis.com/$DRIVER_VERSION/chromedriver_linux64.zip" -P $STORAGE_DIR/chrome
+unzip -o $STORAGE_DIR/chrome/chromedriver_linux64.zip -d $STORAGE_DIR/chrome
 rm $STORAGE_DIR/chrome/chromedriver_linux64.zip
 
-# Add Chrome and ChromeDriver to the PATH
-export PATH="$STORAGE_DIR/chrome/opt/google/chrome:$STORAGE_DIR/chrome:$PATH"
+# be sure to add Chromes location to the PATH as part of your Start Command
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
-# Optionally, you can verify the Chrome installation
-google-chrome --version
-
-# Add your build or install commands here (if any)
-pip install -r requirements.txt
+# add your own build commands...
