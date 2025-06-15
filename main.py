44e456e98b16df4466fc8effc30faf1498cbe175
Chan Ci En <chance3009@outlook.com>
2025-06-15 17:03:35 +0800

    Fix routing

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from app import app as flask_app
 
 @functions_framework.http
 def app(request):
+    # Set the base URL for the Flask app
+    request.environ['SCRIPT_NAME'] = ''
     return flask_app(request.environ, lambda x, y: y)
